c3deaf6bca4a8554722b35b21d0eb9c82a6e3759
ElHombreRosa <gabrielrosado0326@gmail.com>
2024-09-05 12:05:28 -0400

    9/5 Class

---
+++ In Class Practice/9-5-2024.py
@@ -0,0 +1,17 @@
+#This example just takes the numbers as a string, not a value
+#firstNumber = input("Please enter the first number: ")
+#secondNumber = input("Please enter the second number: ")
+#myNumber = firstNumber + secondNumber
+#print(myNumber)
+
+#This example takes the numbers as a value but if the input is not a number (int) it can cause an error 
+# (invalid literal for int() with base 10)
+firstNumber= int(input("Please enter the first number: "))
+secondNumber = int(input("Please enter the second number: "))
+myNumber = firstNumber + secondNumber
+print(myNumber)
+
+#Note: Booleans only take False and True 
+# test = False
+# test2 = True
+
